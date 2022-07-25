94d0f294e100acfeaa75389fd4deda60a10b0ffe
Sebeck10 <souchontheo24@gmail.com>
2022-07-25 15:26:05 +0100

    update proj

--- PostGraphile/FirstCourse/server.sh
+++ PostGraphile/FirstCourse/server.sh
@@ -1,11 +1,10 @@
-npx postgraphile -c postgres:///learn_dev --schema learn 
-  # -c postgres://learn_postgraphile:secret_password@localhost/learn_dev \
-  # -c postgres:///learn_dev \
-  # --schema learn \
-  # --append-plugins @graphile-contrib/pg-simplify-inflector,postgraphile-plugin-connection-filter \
-  # --enhance-graphiql \
-  # --allow-explain \
-  # --watch \
+npx postgraphile \
+-c postgres:///learn_dev \
+--schema learn \
+--append-plugins @graphile-contrib/pg-simplify-inflector \
+--enhance-graphiql \
+--allow-explain \
+--watch 
   # --owner-connection postgres:///learn_dev \
   # --default-role learn_anonymous \
   # --jwt-token-identifier learn.jwt \
